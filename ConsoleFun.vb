9def52ac1a21aae775525b2bd586f41977f93024
jakeb11499@gmail.com <jakeb11499@gmail.com>
2020-08-24 08:18:52 -0600

    Add project files.

---
+++ ConsoleFun.vb
@@ -0,0 +1,10 @@
+﻿
+
+Module ConsoleFun
+
+    Sub Main()
+        Dim userInput As String
+        userInput = Console.ReadLine("Please type some text the press enter...")
+    End Sub
+
+End Module
