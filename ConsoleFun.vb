397872e9654c80df2a45e28898cc316227d327a0
jakeb11499@gmail.com <jakeb11499@gmail.com>
2020-08-24 08:38:49 -0600

    add convet example

--- ConsoleFun.vb
+++ ConsoleFun.vb
@@ -1,10 +1,26 @@
-﻿
+﻿Option Explicit On
+Option Strict On
 
 Module ConsoleFun
 
     Sub Main()
         Dim userInput As String
-        userInput = Console.ReadLine("Please type some text the press enter...")
+        'Console.WriteLine("Please type some text the press enter...")
+        'userInput = Console.ReadLine()
+        'Console.ReadLine()
+        'Console.WriteLine("Thanks!!! You entered: " & userInput & ", Good Work!!")
+        'Console.ReadLine()
+
+        Dim firstNumber As Integer
+        Dim result As Integer
+        Console.WriteLine("Please Enter a Number")
+        userInput = Console.ReadLine()
+        'firstNumber = Console.ReadLine()
+        firstNumber = CInt(userInput)
+        result = 5 + firstNumber
+        Console.WriteLine(result)
+        Console.ReadLine()
+
     End Sub
 
 End Module
